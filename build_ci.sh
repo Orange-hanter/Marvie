6c9a1cf40793b96d7257dc508cf6d7b1689723de
Andrei Shamshur <rushmash@gmail.com>
2019-02-12 22:26:50 +0300

    Store .elf binary in artifact. Calculate sha256 for binaries. Minor tweaks in CI scripts.

--- build_ci.sh
+++ build_ci.sh
@@ -2,19 +2,20 @@
 #
 # expects the following parameters to be passed in this specific order:
 #  - project root folder, e.g. BITBUCKET_CLONE_DIR
+set -e
 
 repository_root_dir="$1"
 artifacts_dir="${repository_root_dir}/artifacts"
 
 # these settings can be changed safely
-build_type="RelWithDebInfo"
+firmware_build_type="RelWithDebInfo"
 
 function configure_cmake ()
 {
   mkdir -p "$1"/build && cd "$1"/build
 
   cmake -GNinja \
-  -DCMAKE_BUILD_TYPE="${build_type}" \
+  -DCMAKE_BUILD_TYPE="${firmware_build_type}" \
   -DSTM32_CHIP=STM32F407VE \
   -DCMAKE_MODULE_PATH="${repository_root_dir}/Firmware/CMake/" \
   -DCMAKE_TOOLCHAIN_FILE="${repository_root_dir}/Firmware/CMake/gcc_stm32.cmake" \
@@ -28,60 +29,76 @@ function configure_cmake ()
 function generate_build_info ()
 {
   mkdir -p "$1"
-  echo "Built on: $(date)" > "$1"/BUILD_INFO
-  echo "Built with: $(/usr/bin/arm-none-eabi-gcc --version | head -1)" >> "$1"/BUILD_INFO
+  echo "Build type: ${firmware_build_type}" > "$1/BUILD_INFO"
+  echo "" >> "$1/BUILD_INFO"
+  echo "Built on: $(date)" >> "$1/BUILD_INFO"
+  echo "Built with: $(/usr/bin/arm-none-eabi-gcc --version | head -1)" >> "$1/BUILD_INFO"
+}
+
+function calc_sha256 ()
+{
+  local filename="$1"
+  (cd $(dirname "${filename}"); sha256sum $(basename "${filename}") > "${filename}.sha256")
 }
 
 function build_firmware ()
 {
-  project_dir="${repository_root_dir}/Firmware"
-  project_build_dir="${project_dir}/build"
+  local firmware_type="$1"
+  local project_dir="${repository_root_dir}/Firmware"
+  local project_build_dir="${project_dir}/build"
 
-  # build H version of firmware
-  configure_cmake "${project_dir}" '-DMODEM_PWRKEY_LEVEL=H'
+  # build
+  configure_cmake "${project_dir}" "-DMODEM_PWRKEY_LEVEL=${firmware_type}"
   marvie_base_version=$(cmake -LA "${project_dir}" | grep MARVIE_FIRMWARE_VERSION_BASE | cut -d'=' -f2)
-  cmake --build "${project_build_dir}" --config "${build_type}" --target marvie_firmware.elf.bin
+  cmake --build "${project_build_dir}" --config "${firmware_build_type}" --target marvie_firmware.elf.bin
 
-  new_filename="${project_build_dir}/marvie.firmware.${marvie_base_version}H.bin"
-  mv "${project_build_dir}/marvie_firmware.elf.bin" "${new_filename}"
-  sha1sum "${new_filename}" > "${new_filename}.sha1"
+  # set proper name of *.elf binary and get its sha256
+  local new_filename_mask="${project_build_dir}/marvie.firmware.${marvie_base_version}${firmware_type}"
+  mv "${project_build_dir}/marvie_firmware.elf" "${new_filename_mask}.elf"
+  calc_sha256 "${new_filename_mask}.elf"
 
-  # build L version of firmware
-  configure_cmake "${project_dir}" '-DMODEM_PWRKEY_LEVEL=L'
-  cmake --build "${project_build_dir}" --config "${build_type}" --target marvie_firmware.elf.bin
+  # set proper name of *.bin binary and get its sha256
+  mv "${project_build_dir}/marvie_firmware.elf.bin" "${new_filename_mask}.bin"
+  calc_sha256 "${new_filename_mask}.bin"
 
-  new_filename="${project_build_dir}/marvie.firmware.${marvie_base_version}L.bin"
-  mv "${project_build_dir}/marvie_firmware.elf.bin" "${new_filename}"
-  sha1sum "${new_filename}" > "${new_filename}.sha1"
+  mkdir -p "${artifacts_dir}/firmware/"
+  for file in "${project_build_dir}"/marvie.firmware*.{bin,elf,sha256}; do
+    mv "${file}" "${artifacts_dir}/firmware/"
+  done
 
   generate_build_info "${artifacts_dir}/firmware"
 
-  # copy artifacts
-  cp "${project_build_dir}"/marvie.firmware*.{bin,sha1} "${artifacts_dir}/firmware"
+  export marvie_base_version
 }
 
 function build_bootloader ()
 {
-  project_dir="${repository_root_dir}/Firmware/Bootloader"
-  project_build_dir="${project_dir}/build"
+  local project_dir="${repository_root_dir}/Firmware/Bootloader"
+  local project_build_dir="${project_dir}/build"
 
+  # build
   configure_cmake "${project_dir}"
-  cmake --build "${project_build_dir}" --config "${build_type}" --target bootloader.elf.bin
+  cmake --build "${project_build_dir}" --config "${firmware_build_type}" --target bootloader.elf.bin
 
-  new_filename="${project_build_dir}/marvie.bootloader.bin"
-  mv "${project_build_dir}/bootloader.elf.bin" "${new_filename}"
-  sha1sum "${new_filename}" > "${new_filename}.sha1"
+  # set proper name of *.bin binary and get its sha256
+  local new_filename_mask="${project_build_dir}/marvie.bootloader"
+  mv "${project_build_dir}/bootloader.elf.bin" "${new_filename_mask}.bin"
+  calc_sha256 "${new_filename_mask}.bin"
+
+  # set proper name of *.bin binary and get its sha256
+  mv "${project_build_dir}/bootloader.elf" "${new_filename_mask}.elf"
+  calc_sha256 "${new_filename_mask}.elf"
+
+  mkdir -p "${artifacts_dir}/bootloader/"
+  for file in "${project_build_dir}"/marvie.bootloader*.{bin,elf,sha256}; do
+    mv "${file}" "${artifacts_dir}/bootloader/"
+  done
 
   generate_build_info "${artifacts_dir}/bootloader"
-
-  # copy artifacts
-  cp "${project_build_dir}"/marvie.bootloader*.{bin,sha1} "${artifacts_dir}/bootloader"
 }
 
-
-mkdir -p "${artifacts_dir}"
-
-build_firmware
+build_firmware 'L'
+build_firmware 'H'
 build_bootloader
 
 echo $marvie_base_version > "${repository_root_dir}"/version
